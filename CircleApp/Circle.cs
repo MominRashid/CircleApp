8ec746bfe037b1b78b6efe9919016587102f2723
mominrashid <momin0444@gmail.com>
2014-09-27 16:58:00 +0600

    ok

---
+++ CircleApp/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Circle
+    {
+        public double radious;
+     
+        public double pievalue = Math.PI;
+
+        public double GetDiameter(double radious)
+
+    {
+        return 2*radious;
+    }
+
+
+    public double GetPerimeter(double radious)
+    {
+        return 2*radious*pievalue;
+
+    }
+
+        public double GetArea(double radious)
+        {
+            return 2*radious*radious;
+
+        }
+    }
+}
---
+++ CircleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle aCircle = new Circle();
+
+            Console.Write(" Enter your Input Radious Value ");
+            aCircle.radious = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine(" Area is " + aCircle.GetArea(aCircle.radious));
+
+            Console.ReadKey();
+
+        }
+    }
+}
